ab61ec97041989307ecd8825be96a5d75ffc96aa
justfungx <justfungx@hotmail.com>
2016-09-13 16:44:02 +0800

    v1

---
+++ app/src/main/java/tw/org/iii/mypen/MyView.java
@@ -0,0 +1,38 @@
+package tw.org.iii.mypen;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by user on 2016/9/13.
+ */
+public class MyView extends View{
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint p = new Paint();
+        p.setColor(Color.BLUE);
+        p.setStrokeWidth(4);
+        canvas.drawLine(0,0,100,100,p);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float ex = event.getX(), ey = event.getY();
+        Log.d("DK", "onTouchEvent:" + ex + " x " + ey);
+        return true;
+
+    }
+
+}
