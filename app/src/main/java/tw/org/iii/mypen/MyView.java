1b0fdf1113da4961f7b60a7465aab820f305c40f
justfungx <justfungx@hotmail.com>
2016-09-14 14:48:11 +0800

    v9

--- app/src/main/java/tw/org/iii/mypen/MyView.java
+++ app/src/main/java/tw/org/iii/mypen/MyView.java
@@ -10,6 +10,7 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -19,19 +20,18 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * Created by user on 2016/9/13.
+ * Created by brad on 2016/9/13.
  */
 public class MyView extends View {
     private LinkedList<LinkedList<HashMap<String,Float>>> lines;
     private Resources res;
     private boolean isInit;
     private int viewW, viewH;
-    private Bitmap bmpFly ,bmpBg;
+    private Bitmap bmpBall, bmpBg;
     private Matrix matrix;
     private Timer timer;
-    private float flyX ,flyY,flyW,flyH,dx,dy;
-
-
+    private float ballX, ballY, ballW, ballH, dx, dy;
+    private GestureDetector gd;
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -40,33 +40,51 @@ public class MyView extends View {
         matrix = new Matrix();
         timer = new Timer();
 
+        gd = new GestureDetector(context, new MyGDListener());
+
     }
 
-    Timer getTimer(){return timer;}
-
-        private void init(){
-
-            viewW = getWidth(); viewH = getHeight();
-            float flyW = viewW / 8f, flyH = flyW;
-
-            bmpBg = BitmapFactory.decodeResource(res, R.drawable.bg);
-            bmpBg = resizeBitmap(bmpBg, viewW,viewH);
-
-            bmpFly = BitmapFactory.decodeResource(res, R.drawable.fly);
-            bmpFly = resizeBitmap(bmpFly,flyW,flyH);
-            dx = dy = 10;
-
-            timer.schedule(new RefreshView(), 0, 40);
-            timer.schedule(new FlyTask(), 1000, 100);
-                isInit = true;
+    private class MyGDListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onDown(MotionEvent e) {
+            //Log.d("brad", "onDown");
+            return true; //super.onDown(e);
         }
 
-    public  Bitmap resizeBitmap(Bitmap src ,float newW ,float newH){
-        matrix.reset();
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d("brad", "onFling:" + velocityX + "x" + velocityY);
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
 
-        matrix.postScale(newW/src.getWidth(), newW/src.getHeight());
-        bmpFly = Bitmap.createBitmap(src,0,0,src.getWidth(),src.getHeight(),matrix, false);
-        return bmpFly;
+
+
+
+    Timer getTimer(){return timer;}
+    private void init(){
+        viewW = getWidth(); viewH = getHeight();
+        ballW = viewW / 8f; ballH = ballW;
+
+        bmpBg = BitmapFactory.decodeResource(res, R.drawable.bg);
+        bmpBg = resizeBitmap(bmpBg, viewW,viewH);
+
+       bmpBall = BitmapFactory.decodeResource(res, R.drawable.ball);
+        bmpBall = resizeBitmap(bmpBall, ballW,ballH);
+
+        dx = dy = 10;
+
+        timer.schedule(new RefreshView(), 0, 40);
+        timer.schedule(new BallTask(), 1000, 100);
+
+        isInit = true;
+    }
+
+    private Bitmap resizeBitmap(Bitmap src, float newW, float newH){
+        matrix.reset();
+        matrix.postScale(newW/src.getWidth(), newH/src.getHeight());
+        bmpBall = Bitmap.createBitmap(src,0,0,src.getWidth(),src.getHeight(),matrix, false);
+        return bmpBall;
     }
 
     @Override
@@ -74,19 +92,18 @@ public class MyView extends View {
         super.onDraw(canvas);
         if (!isInit) init();
 
+        canvas.drawBitmap(bmpBg, 0,0,null);
 
-        canvas.drawBitmap(bmpFly, 0,0,null);
-        canvas.drawBitmap(bmpFly, flyX, flyY,null);
-
+        canvas.drawBitmap(bmpBall, ballX, ballY,null);
 
 
         Paint p = new Paint();
         p.setColor(Color.BLUE);
         p.setStrokeWidth(4);
         for (LinkedList<HashMap<String,Float>> line:lines) {
-            for (int i = 1; i <= line.size(); i++) {
-                canvas.drawLine(line.get(i - 1).get("x"), line.get(i - 1).get("y")
-                        , line.get(i).get("x"), line.get(i).get("y"), p);
+            for (int i = 1; i < line.size(); i++) {
+                canvas.drawLine(line.get(i - 1).get("x"), line.get(i - 1).get("y"),
+                        line.get(i).get("x"), line.get(i).get("y"), p);
             }
         }
     }
@@ -98,37 +115,34 @@ public class MyView extends View {
             postInvalidate();
         }
     }
-
-
-    private class FlyTask extends TimerTask {
+    private class BallTask extends TimerTask {
         @Override
         public void run() {
-            if (flyX<0 || flyX +flyW > viewW) dx *= -1;
-            if (flyY<0 || flyY + flyH > viewH) dy *= -1;
-            flyX += dx; flyY += dy;
+            if (ballX<0 || ballX + ballW > viewW) dx *= -1;
+            if (ballY<0 || ballY + ballH > viewH) dy *= -1;
+            ballX += dx; ballY += dy;
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         float ex = event.getX(), ey = event.getY();
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            doTouchDown(ex,ey);
-            Log.d("DK", "Down:" + ex + " x " + ey);
-
-        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            doTouchMove(ex,ey);
-            Log.d("DK", "Move:" + ex + " x " + ey);
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            doTouchDown(ex, ey);
+        }else if (event.getAction() == MotionEvent.ACTION_MOVE){
+            doTouchMove(ex, ey);
         }
-        return true;
+        //return true;
+        return gd.onTouchEvent(event);
     }
-    public void doTouchDown (float x,float y){
+
+    private void doTouchDown(float x, float y){
         LinkedList<HashMap<String,Float>> line =
                 new LinkedList<>();
         lines.add(line);
         addPoint(x,y);
     }
-    public void doTouchMove(float x ,float y){
+    private void doTouchMove(float x, float y){
         addPoint(x,y);
     }
     private void addPoint(float x, float y){
@@ -136,6 +150,9 @@ public class MyView extends View {
                 new HashMap<>();
         point.put("x", x); point.put("y", y);
         lines.getLast().add(point);
+        Log.d("DK","point");
         invalidate();
     }
+
+
 }
