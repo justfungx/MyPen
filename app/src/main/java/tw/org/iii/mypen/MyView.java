c2c0fa53347f9ce2c234768ec62315853e6d5d82
justfungx <justfungx@hotmail.com>
2016-09-14 10:13:51 +0800

    v4

--- app/src/main/java/tw/org/iii/mypen/MyView.java
+++ app/src/main/java/tw/org/iii/mypen/MyView.java
@@ -9,12 +9,18 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  * Created by user on 2016/9/13.
  */
 public class MyView extends View {
+    private LinkedList<HashMap<String,Float>> line;
+
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        line =  new LinkedList<>();
 
     }
 
@@ -24,17 +30,37 @@ public class MyView extends View {
         Paint p = new Paint();
         p.setColor(Color.BLUE);
         p.setStrokeWidth(4);
-        canvas.drawLine(0, 0, 100, 500, p);
+        for(int i = 1 ; i <=line.size() ;i++) {
+            canvas.drawLine(line.get(i-1).get("x"),line.get(i-1).get("y")
+                    , line.get(i).get("x"),line.get(i).get("y"), p);
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         float ex = event.getX(), ey = event.getY();
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            Log.d("brad", "Down:" + ex + " x " + ey);
+            doTouchDown(ex,ey);
+            Log.d("DK", "Down:" + ex + " x " + ey);
+
         } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            Log.d("brad", "Move:" + ex + " x " + ey);
+            doTouchMove(ex,ey);
+            Log.d("DK", "Move:" + ex + " x " + ey);
         }
         return true;
+    }
+    public void doTouchDown (float x,float y){
+        HashMap<String,Float>point =
+            new HashMap<>();
+        point.put("x", x); point.put("y", y);
+        line.add(point);
+        invalidate();
+    }
+    public void doTouchMove(float x ,float y){
+        HashMap<String,Float>point =
+                new HashMap<>();
+        point.put("x", x); point.put("y", y);
+        line.add(point);
+        invalidate();
     }
 }
