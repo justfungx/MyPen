b82cc8a33cc24f9423b3b79f13a43a859653d664
justfungx <justfungx@hotmail.com>
2016-09-14 10:29:29 +0800

    v5

--- app/src/main/java/tw/org/iii/mypen/MyView.java
+++ app/src/main/java/tw/org/iii/mypen/MyView.java
@@ -1,6 +1,9 @@
 package tw.org.iii.mypen;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -16,23 +19,29 @@ import java.util.LinkedList;
  * Created by user on 2016/9/13.
  */
 public class MyView extends View {
-    private LinkedList<HashMap<String,Float>> line;
+    private LinkedList<LinkedList<HashMap<String,Float>>> lines;
+    private Resources res;
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        line =  new LinkedList<>();
+        lines =  new LinkedList<>();
+        res = context.getResources();
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        Bitmap bmpBall = BitmapFactory.decodeResource(res, R.drawable.fly);
+        canvas.drawBitmap(bmpBall, 0,0,null);
         super.onDraw(canvas);
         Paint p = new Paint();
         p.setColor(Color.BLUE);
         p.setStrokeWidth(4);
-        for(int i = 1 ; i <=line.size() ;i++) {
-            canvas.drawLine(line.get(i-1).get("x"),line.get(i-1).get("y")
-                    , line.get(i).get("x"),line.get(i).get("y"), p);
+        for (LinkedList<HashMap<String,Float>> line:lines) {
+            for (int i = 1; i <= line.size(); i++) {
+                canvas.drawLine(line.get(i - 1).get("x"), line.get(i - 1).get("y")
+                        , line.get(i).get("x"), line.get(i).get("y"), p);
+            }
         }
     }
 
@@ -50,17 +59,19 @@ public class MyView extends View {
         return true;
     }
     public void doTouchDown (float x,float y){
-        HashMap<String,Float>point =
-            new HashMap<>();
-        point.put("x", x); point.put("y", y);
-        line.add(point);
-        invalidate();
+        LinkedList<HashMap<String,Float>> line =
+                new LinkedList<>();
+        lines.add(line);
+        addPoint(x,y);
     }
     public void doTouchMove(float x ,float y){
-        HashMap<String,Float>point =
+        addPoint(x,y);
+    }
+    private void addPoint(float x, float y){
+        HashMap<String,Float> point =
                 new HashMap<>();
         point.put("x", x); point.put("y", y);
-        line.add(point);
+        lines.getLast().add(point);
         invalidate();
     }
 }
